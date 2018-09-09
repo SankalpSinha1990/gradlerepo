867b2b2b762af45a994b8116f6682454f8d2b701
trainingport <32031618+trainingport@users.noreply.github.com>
2018-09-09 23:09:13 +0530

    Add files via upload

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,13 @@
+public class HelloWorld {
+	
+	
+	public boolean printHello() {
+		
+		return true;
+	}
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Hello World!");
+	}
+}
